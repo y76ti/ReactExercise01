eab6d25ac95f6e0ac79573443c9178abd14e776c
Jessy <jessymarolle@gmail.com>
2024-06-12 14:46:34 +0200

    propsOk

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
 
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+      
+  },
+  {
+      name: "mew",
+  },
+];
 
 function App() {
   
@@ -8,7 +18,7 @@ function App() {
   return (
 <>
       <div>
-        <PokemonCard/>
+        <PokemonCard pokemon= {pokemonList[0]}/>
       </div>
 </>
 )}
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,6 @@
-function PokemonCard(){
+function PokemonCard({pokemon}){
     
 
-    const pokemonList = [
-        {
-            name: "bulbasaur",
-            imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            
-        },
-        {
-            name: "mew",
-        },
-    ];
-    
-    const pokemon = pokemonList[0]
     return(
 
     <figure>
